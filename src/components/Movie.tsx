16a4082dd1987912f9bc3af2a830dce59c79eefa
rmfpdlxmtidl <gwak2837@kakao.com>
2020-09-10 19:53:18 +0900

    Add some interfaces
    여러 인터페이스 추가

--- src/components/Movie.tsx
+++ src/components/Movie.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { IMovie } from "interfaces";
+// import { IMovie } from "interfaces";
 
-function Movie({ id, name }: IMovie) {
+function Movie({ id, name }: { id: number; name: string }) {
   return (
     <div>
       {" "}
--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,19 +1,52 @@
 export interface IMovie {
   id: number;
   name: string;
-  rating?: number;
+  rating: number;
 }
 
-export interface IUserContext {
-  user: IUser;
-  setUser: (user: IUser) => IUser;
+export interface IMovieData {
+  movie: IMovie;
 }
 
+export interface IMovieVars {
+  id: number;
+}
+
+export interface IMoviesData {
+  movies: IMovie[];
+}
+export interface ILogin {
+  ID: string;
+  passwordHash: string;
+}
 export interface IUser {
-  id?: number;
-  name?: string;
-  ID?: string;
-  passwordHash?: string;
-  role?: string[];
-  token?: string;
+  id: number;
+  name: string;
+  ID: string;
+  passwordHash: string;
+  role: string[];
+  token: string;
+}
+
+export interface ILoginData {
+  login: IUser;
+}
+
+export interface ILoginVars {
+  ID: string;
+  password: string;
+}
+
+export interface ILogoutData {
+  logout: boolean;
+}
+
+export interface ISignupData {
+  signup: boolean;
+}
+
+export interface ISignupVars {
+  name: string;
+  ID: string;
+  password: string;
 }
