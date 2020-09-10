f5b6bdbc7183377ca71021b5b40f4f3ff4426ffc
rmfpdlxmtidl <gwak2837@kakao.com>
2020-09-10 19:51:04 +0900

    Migrate creating Apollo Client code from index.tsx to App.tsx
    Apollo 클라이언트를 생성하는 코드를 index.tsx에서 App.tsx로 옮김

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import React from "react";
 import Router from "Router";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/",
+  cache: new InMemoryCache(),
+});
 
 function App() {
-  return <Router />;
+  return (
+    <ApolloProvider client={client}>
+      <Router />
+    </ApolloProvider>
+  );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 import App from "App";
-
-const client = new ApolloClient({
-  uri: "http://localhost:4000/",
-  cache: new InMemoryCache(),
-});
 
 ReactDOM.render(
   <React.StrictMode>
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
