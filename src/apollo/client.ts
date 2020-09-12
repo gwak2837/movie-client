f00f1bc97395731b4d436752f3ff8097956b181e
rmfpdlxmtidl <gwak2837@kakao.com>
2020-09-12 18:41:38 +0900

    Update client.ts

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -7,7 +7,7 @@ const httpLink = createHttpLink({
   uri: "http://localhost:4000/graphql",
 });
 
-const authLink = setContext((_: GraphQLRequest, { headers }) => {
+const contextSetter = (_: GraphQLRequest, { headers }: any) => {
   // get the authentication token from local storage if it exists
   const token = currentUserVar()?.token;
   // return the headers to the context so httpLink can read them
@@ -17,9 +17,9 @@ const authLink = setContext((_: GraphQLRequest, { headers }) => {
       authorization: token ? token : "",
     },
   };
-});
+};
 
 export const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: cache,
+  link: setContext(contextSetter).concat(httpLink),
+  cache,
 });
