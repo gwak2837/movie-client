0ee870d2d63edb39a7e698f7529a2e502b615f55
rmfpdlxmtidl <gwak2837@kakao.com>
2020-09-12 14:35:47 +0900

    Add Loading and Error components

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { gql, useQuery } from "@apollo/client";
-import { IMovieVars, IMovieData } from "interfaces";
+import { IMovieData, IMovieVars } from "interfaces";
+import Error from "components/Error";
+import Loading from "components/Loading";
 
 const GET_MOVIE = gql`
   query getMovie($id: Int!) {
@@ -14,13 +16,13 @@ const GET_MOVIE = gql`
 `;
 
 function Detail() {
-  const { id } = useParams();
+  const { id } = useParams<{ id: string }>();
   const { loading, error, data } = useQuery<IMovieData, IMovieVars>(GET_MOVIE, {
     variables: { id: Number(id) },
   });
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>An error occurred</p>;
+  if (loading) return <Loading />;
+  if (error) return <Error msg={error.message} />;
 
   // 서버로부터 받은 데이터가 있으면 영화 정보를 반환하고
   // 없으면 'No Detail...' 반환
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { gql, useQuery } from "@apollo/client";
 import Movie from "components/Movie";
 import { IMoviesData } from "interfaces";
+import Error from "components/Error";
+import Loading from "components/Loading";
 
 const GET_MOVIE = gql`
   query {
@@ -15,8 +17,8 @@ const GET_MOVIE = gql`
 function Home() {
   const { loading, error, data } = useQuery<IMoviesData>(GET_MOVIE);
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>An error occurred</p>;
+  if (loading) return <Loading />;
+  if (error) return <Error msg={error.message} />;
 
   return (
     <div>
